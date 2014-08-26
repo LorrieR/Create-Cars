21a460982041574afc3147fdba023f4b2d3d1689
LorrieR <lawrencerosenberg@icloud.com>
2014-08-25 23:29:00 -0400

    Unit 2 Project Upload #1

---
+++ Carfactory.swift
@@ -0,0 +1,16 @@
+//
+//  Carfactory.swift
+//  Create Cars
+//
+//  Created by Lawrence Rosenberg on 2014-08-25.
+//  Copyright (c) 2014 Lawrence Rosenberg. All rights reserved.
+//
+
+import Foundation
+
+class CarFactory {
+    var name = ""
+    var color = ""
+    var horsepower = 0
+    var automaticOption = false
+}
